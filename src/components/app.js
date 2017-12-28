c93b8a0c9379f3f9ed0fa2eff8236fcc42b6be07
Ryan Clemente <rclemente@zendesk.com>
2017-12-28 14:45:40 +0800

    Fix typo

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 
-import BooKList from '../containers/book-list';
+import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
